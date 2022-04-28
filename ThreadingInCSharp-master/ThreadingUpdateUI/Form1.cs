a5279b49d44555d26759134a9009d67fdaa393b5
Mr. M <judson.murray@nbc.ca>
2022-04-28 09:24:11 -0300

    Added Multi-threading implementation for GO button Event Handler

--- ThreadingInCSharp-master/ThreadingUpdateUI/Form1.cs
+++ ThreadingInCSharp-master/ThreadingUpdateUI/Form1.cs
@@ -18,10 +18,41 @@ namespace ThreadingUpdateUI
             InitializeComponent();
         }
 
-      
+        List<Label> threadLabels = new List<Label>();
+
         private void btnGo_Click(object sender, EventArgs e)
         {
+            if (threadLabels.Count < 100)
+            {
+                int.TryParse(textBox1.Text, out int i);
+                int.TryParse(textBox2.Text, out int sleep);
 
+                Label l = new Label();
+                l.Text = DateTime.Now.ToLongTimeString();
+
+                threadLabels.Add(l);
+
+                l.Top = (panel1.Controls.Count) * l.Height + 20;
+                l.Left = 5;
+
+                this.panel1.Controls.Add(l);
+
+                Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            l.Text = j.ToString();
+                        }
+                        );
+
+                        System.Threading.Thread.Sleep(sleep);
+
+                    }
+                });
+
+            }
         }
 
         private void btnGoNoThread_Click(object sender, EventArgs e)
@@ -36,6 +67,7 @@ namespace ThreadingUpdateUI
             for (int j = 0; j <= i; j++)
             {
                 lblCounter.Text = j.ToString();
+                lblCounter.Refresh();
                 System.Threading.Thread.Sleep(sleep);
             }
 
@@ -49,7 +81,9 @@ namespace ThreadingUpdateUI
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             //We need to kill any running threads
-            
+
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+
         }
     }
 }
